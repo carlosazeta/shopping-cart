fe0df415e0a991cd51e06d75821a7b1c6a977a20
Carlos Agundez <internetcharlytech@gmail.com>
2023-03-15 03:34:16 +0100

    Show products with mock and add filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import './App.css'
+import { useState } from 'react'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Products } from './components/Products'
+import { products as initialProducts } from './mocks/products.json'
 
+function useFilters() {
+	const [filters, setFilters] = useState({
+		category: 'all',
+		minPrice: 0,
+	})
+
+	const filterProducts = (products) => {
+		return products.filter((product) => {
+			return (
+				product.price >= filters.minPrice &&
+				(filters.category === 'all' || product.category === filters.category)
+			)
+		})
+	}
+
+	return { filterProducts, setFilters }
+}
 function App() {
+	const [products] = useState(initialProducts)
+	const { filterProducts, setFilters } = useFilters()
+
+	const filteredProducts = filterProducts(products)
+
 	return (
 		<>
-			<p>Hello world!</p>
+			<Header changeFilters={setFilters} />
+			<Products products={filteredProducts} />
+			<Footer />
 		</>
 	)
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export function Footer() {
+	return (
+		<footer className='footer'>
+			<h4>
+				React shop with React 🐱‍💻 — <span>@internetcharly</span>
+			</h4>
+			<h5>This is a prototype, enjoy it!</h5>
+		</footer>
+	)
+}
