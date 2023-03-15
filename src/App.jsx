26cd27c714dbae58bfb2a87b0140bcb96e6e2ebf
Carlos Agundez <internetcharlytech@gmail.com>
2023-03-15 03:47:29 +0100

    Add debugger footer in development eviroment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Products } from './components/Products'
+import { IS_DEVELOPMENT } from './config'
 import { products as initialProducts } from './mocks/products.json'
 
 function useFilters() {
@@ -19,11 +20,11 @@ function useFilters() {
 		})
 	}
 
-	return { filterProducts, setFilters }
+	return { filters, filterProducts, setFilters }
 }
 function App() {
 	const [products] = useState(initialProducts)
-	const { filterProducts, setFilters } = useFilters()
+	const { filters, filterProducts, setFilters } = useFilters()
 
 	const filteredProducts = filterProducts(products)
 
@@ -31,7 +32,7 @@ function App() {
 		<>
 			<Header changeFilters={setFilters} />
 			<Products products={filteredProducts} />
-			<Footer />
+			{IS_DEVELOPMENT && <Footer filters={filters} />}
 		</>
 	)
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import './Footer.css'
 
-export function Footer() {
+export function Footer({ filters }) {
 	return (
 		<footer className='footer'>
-			<h4>
+			{JSON.stringify(filters, null, 2)}
+			{/* <h4>
 				React shop with React 🐱‍💻 — <span>@internetcharly</span>
 			</h4>
-			<h5>This is a prototype, enjoy it!</h5>
+			<h5>This is a prototype, enjoy it!</h5> */}
 		</footer>
 	)
 }
